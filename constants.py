e1e8ea302eaf62925a68caf5416bdf77259f625b
asil004 <72306088+asil004@users.noreply.github.com>
2023-08-19 16:50:57 +0500

    asilbek update 3

--- constants.py
+++ constants.py
@@ -46,3 +46,65 @@ Xufton: {xufton}
 
 Manba: islom.uz
 """
+
+WEEK_PRAY_TIME = """
+Bir haftalik namoz vaqtlari {start_date} - {end_date}.
+Hudud: {hudud}.
+
+☪️ Namoz vaqtlari:
+Kun: <i><b>Dushanba</b></i>
+Bomdod: {dush_bomdod}
+Quyosh: {dush_quyosh}
+Peshin: {dush_peshin}
+Asr: {dush_asr}
+Shom: {dush_shom}
+Xufton: {dush_xufton}
+
+Kun: <i><b>Seshanba</b></i>
+Bomdod: {sesh_bomdod}
+Quyosh: {sesh_quyosh}
+Peshin: {sesh_peshin}
+Asr: {sesh_asr}
+Shom: {sesh_shom}
+Xufton: {sesh_xufton}
+
+Kun: <i><b>Chorshanba</b></i>
+Bomdod: {chor_bomdod}
+Quyosh: {chor_quyosh}
+Peshin: {chor_peshin}
+Asr: {chor_asr}
+Shom: {chor_shom}
+Xufton: {chor_xufton}
+
+Kun: <i><b>Payshanba</b></i>
+Bomdod: {pay_bomdod}
+Quyosh: {pay_quyosh}
+Peshin: {pay_peshin}
+Asr: {pay_asr}
+Shom: {pay_shom}
+Xufton: {pay_xufton}
+
+Kun: <i><b>Juma</b></i>
+Bomdod: {juma_bomdod}
+Quyosh: {juma_quyosh}
+Peshin: {juma_peshin}
+Asr: {juma_asr}
+Shom: {juma_shom}
+Xufton: {juma_xufton}
+
+Kun: <i><b>Shanba</b></i>
+Bomdod: {shan_bomdod}
+Quyosh: {shan_quyosh}
+Peshin: {shan_peshin}
+Asr: {shan_asr}
+Shom: {shan_shom}
+Xufton: {shan_xufton}
+
+Kun: <i><b>Yanshanba</b></i>
+Bomdod: {yak_bomdod}
+Quyosh: {yak_quyosh}
+Peshin: {yak_peshin}
+Asr: {yak_asr}
+Shom: {yak_shom}
+Xufton: {yak_xufton}
+"""
