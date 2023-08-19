bce4149b402cfb613b0d51e2b474e13e2b109ce4
asil004 <72306088+asil004@users.noreply.github.com>
2023-08-19 15:23:38 +0500

    asilbek update 2

---
+++ routers/start.py
@@ -0,0 +1,9 @@
+from aiogram import Router, types
+from aiogram.filters import CommandStart
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start_handler(message: types.Message):
+    await
