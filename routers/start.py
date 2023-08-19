6fa0525f35c8a34270e90c2e2396f70a0dd62173
asil004 <72306088+asil004@users.noreply.github.com>
2023-08-19 16:56:26 +0500

    asilbek update 3

--- routers/start.py
+++ routers/start.py
@@ -4,6 +4,7 @@ from aiogram import Router, types
 from aiogram.filters import CommandStart
 from constants import SALAM_TEXT
 from dotenv import load_dotenv
+from keyboards.keyboards import main_menu
 
 router = Router()
 
@@ -13,4 +14,4 @@ load_dotenv('.env')
 @router.message(CommandStart())
 async def start_handler(message: types.Message):
     await message.answer_sticker(sticker=os.getenv('SALAM_STICKER'))
-    await message.answer(text=SALAM_TEXT)
+    await message.answer(text=SALAM_TEXT, reply_markup=main_menu())
