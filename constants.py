03a3caaa5c330fe0d4e4081b62df25e9fdd1a9c6
asil004 <72306088+asil004@users.noreply.github.com>
2023-08-19 16:40:12 +0500

    asilbek update 3

--- constants.py
+++ constants.py
@@ -30,3 +30,19 @@ Namoz vaqtlari botiga xush kelibsiz.
 
 ©Barcha malumotlar islom.uz web-saytidan olingan!
 """
+
+TODAY_PRAY_TEXT = """
+Bugun {day}-{moth}.
+Hudud: {hudud}.
+
+☪️ Namoz vaqtlari:
+
+Bomdod: {bomdod}
+Quyosh: {quyosh}
+Peshin: {peshin}
+Asr: {asr}
+Shom: {shom}
+Xufton: {xufton}
+
+Manba: islom.uz
+"""
