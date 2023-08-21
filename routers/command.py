a6411c5ffa133b719518223784efde723221c919
asil004 <72306088+asil004@users.noreply.github.com>
2023-08-21 18:50:50 +0500

    asilbek update 5

--- routers/command.py
+++ routers/command.py
@@ -1,6 +1,5 @@
 from aiogram import Router, F
 from aiogram import types
-
 from app.methods import get_location, get_prayer_time
 from constants import (
     CONFIRMITION,
@@ -8,7 +7,7 @@ from constants import (
     CONFIRMITION_TEXT,
     SELECT,
     regions,
-    LOCATION_BACK, CONFIRM_LOC
+    LOCATION_BACK, CONFIRM_LOC, TODAY
 )
 from keyboards.keyboards import (
     main_menu,
@@ -31,6 +30,10 @@ async def my_location(message: types.Message):
 
 @router.message(F.text == CONFIRMITION)
 async def confirmition(message: types.Message):
+    global city
+    with open('users_location.txt', 'w') as file:
+        file.writelines(f'{message.from_user.id}={city}')
+    city = ''
     await message.answer(text=CONFIRMITION_TEXT, reply_markup=when())
 
 
@@ -51,4 +54,11 @@ async def back_handler(message: types.Message):
 
 @router.message(F.text.in_(regions))
 async def get_pray_times(message: types.Message):
-    await message.answer(text=get_prayer_time(message.text))
+    await message.answer(text=CONFIRM_LOC.format(city=message.text), reply_markup=send_location())
+    global city
+    city = message.text
+
+
+@router.message(F.text == TODAY)
+async def get_pray_times_today(message: types.Message):
+    pass
